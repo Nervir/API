ff7fe629f42c3b4280e4a18eb3f8958924bd2716
Adam Stawecki <x.stawecki@gmail.com>
2021-12-16 16:57:01 +0100

    Update

--- Scripts/app.py
+++ Scripts/app.py
@@ -19,8 +19,10 @@ with open('rates.csv', 'w', newline='', encoding='utf-8') as csvfile:
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def test():
-    return render_template("test.html", data=data)
-
-
+    if request.method == 'GET':
+        return render_template("test.html", data=data)
+    if request.method == 'POST':
+        cost = data[0]['rates'][request.form[i]]['bid'] * request.form['amount']
+        return render_template("test.html", data=data, cost=cost)
