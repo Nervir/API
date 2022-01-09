20d00c106eeeae7bbd8cfa7829fa54a91a8f3127
Adam Stawecki <x.stawecki@gmail.com>
2022-01-09 18:32:14 +0100

    Update

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-import json
+import requests
+import csv
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
 data = {"data": {
   "id": 1,
   "name": "Something",
@@ -6,11 +10,10 @@ data = {"data": {
   }
 }
 
-import os
-import requests
+
 response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
 data = response.json()
-import csv
+
 with open('rates.csv', 'w', newline='', encoding='utf-8') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
     for i in data[0]['rates']:
@@ -19,9 +22,11 @@ with open('rates.csv', 'w', newline='', encoding='utf-8') as csvfile:
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+
 @app.route("/", methods = ['GET'])
 def test():
-    print(data)
+    print(data[0]['rates'])
     if request.method == 'GET':
         return render_template("test.html", data=data)
 
@@ -29,6 +34,8 @@ def test():
 @app.route("/home", methods = ['POST'])
 def home():
     if request.method == 'POST':
-        print(request.form)
-        cost = data[0]['rates'][request.form[i]]['bid'] * request.form['amount']
+        cost = data[0]['rates'][request.form[i]]['bid'] #* request.form['amount']
         return render_template("test.html", data=data, cost=cost)
+
+if __name__ == "__main__":
+    app.run(debug=True)
